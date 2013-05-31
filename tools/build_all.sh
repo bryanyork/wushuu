7c9b6e9ea66ad770885705e0b0c342df8fcb3ab0
James Fang <james.fang.cn@gmail.com>
2013-05-31 15:26:04 +0800

    Default build are shared & 32bit

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -2,6 +2,12 @@
 
 rm -rf $(pwd)/../3rdparty && rm -rf build
 
+# have to integrate device support
+export WS_BUILD_SHARED=true
+# hikvision only have 32bit decode SDK for linux
+export WS_COMPILE_FLAGS=-m32
+export WS_COMPILE_FLAGS=-m32
+
 . build_pkg-config.sh
 . build_yasm.sh
 . build_ffmpeg.sh
