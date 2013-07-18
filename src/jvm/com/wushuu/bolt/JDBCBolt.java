e5fd8d9e58b22fdeea9f9a89f1d9e2bf2d3b0881
James Fang <james.fang.cn@gmail.com>
2013-07-18 18:04:32 +0800

    jna for bgfgcb, and test

---
+++ src/jvm/com/wushuu/bolt/JDBCBolt.java
@@ -0,0 +1,33 @@
+package com.wushuu.bolt;
+
+import java.util.Map;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.task.TopologyContext;
+
+public class JDBCBolt extends BaseBasicBolt {
+  Connection  mysql = null;
+
+  @Override
+  public void prepare(Map stormConf, TopologyContext context) {
+    try {
+      mysql = DriverManager.getConnection("jdbc:mysql://192.168.2.181/ranyang?autoReconnect=true", "root", "woyoadmin");
+    } catch (SQLException e) {
+    }
+  }
+
+  @Override
+  public void execute(Tuple tup, BasicOutputCollector collector) {
+    System.out.println(tup);
+  }
+
+  @Override
+  public void declareOutputFields(OutputFieldsDeclarer ofd) {
+  }
+}
