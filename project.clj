84163be146fbfc9f3a19e5402418e18e607dc902
James Fang <54740235@qq.com>
2013-07-18 22:41:05 +0800

    use relative path, upgrade jna to 4.0

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject wushuu "0.0.1-SNAPSHOT"
   :java-source-paths ["src/jvm"]
   :resource-paths ["res"]
-  :jvm-opts ["-Djna.library.path=/home/jamesf/work/wushuu/res/native"]
+  :jvm-opts ["-Djna.library.path=res/native"]
   :aot :all
   :dependencies [
-                  [net.java.dev.jna/jna "3.5.2"]
+                  [net.java.dev.jna/jna "4.0.0"]
                   [mysql/mysql-connector-java "5.1.25"]
-                  [org.apache.commons/commons-io "1.3.2"]
+                  [commons-io/commons-io "2.4"]
                   [org.apache.commons/commons-vfs2 "2.0"]]
   :profiles {:dev
               {:dependencies [ 
