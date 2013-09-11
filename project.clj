5f4a94e33e981f3dd22aecd46f517fd77c09d297
James Fang <none@none.no>
2013-09-11 12:49:06 +0800

    lein -> pom -> m2eclipse; storm should be provided

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject com.wushuu/wushuu "0.0.1-SNAPSHOT"
-  :source-paths ["src/clojure"]
   :java-source-paths ["src/main/java"]
   :javac-options ["-target" "1.7" "-source" "1.7"]
-  :resource-paths ["src/resource"]
+  :resource-paths ["src/resource" "target/resource"]
   :junit ["src/test/java"]
   :aot :all
   :dependencies [
@@ -12,17 +11,19 @@
                   [mysql/mysql-connector-java "5.1.25"]
                   [org.jdbi/jdbi "2.49"]
                 ]
-  :profiles {:dev
+  :profiles {:provided
               {:dependencies [ 
-                               [org.clojure/clojure "1.4.0"]
-                               [junit/junit "4.11"]
                                [storm "0.8.2"]
+                             ]
+              }
+             :test
+              {:dependencies [ 
+                               [junit/junit "4.11"]
                              ]
               }
             }
   :min-lein-version "2.2.0"
   :plugins [
-             [lein-idefiles "0.2.0"]
              [lein-junit "1.1.3"]
            ]
   )
